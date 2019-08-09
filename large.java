98640b4c5486cda33f20637948b56be720f8225d
kameshvar <25860419+kameshvar@users.noreply.github.com>
2019-08-09 13:52:36 +0530

    Create large.java

---
+++ large.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+class large
+{
+  public static void main(String args[])
+  {
+    int num1,num2,num3;
+    Scanner in = new Scanner(System.in);
+    num1 = in.nextInt();
+    num2 = in.nextInt();
+    num3 = in.nextInt();
+ 
+    if (num1 > num2 && num1 > num3)
+      System.out.println(num1);
+    else if (num2 > num1 && num2 > num3)
+      System.out.println(y);
+    else  (num3 > num1 && num3 > num2)
+      System.out.println(z);
+   }
+}
