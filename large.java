3c4bb4fdd77e7f368d577457e608dc759f54f2db
kameshvar <25860419+kameshvar@users.noreply.github.com>
2019-08-09 14:23:16 +0530

    Update large.java

--- large.java
+++ large.java
@@ -1,19 +1,27 @@
-import java.util.Scanner;
-public class large
+import java.util.*;
+class large
 {
-  public static void main(String args[])
-  {
-    int num1,num2,num3;
-    Scanner in = new Scanner(System.in);
-    num1 = in.nextInt();
-    num2 = in.nextInt();
-    num3 = in.nextInt();
- 
-    if (num1 > num2 && num1 > num3)
-      System.out.println(num1);
-    else if (num2 > num1 && num2 > num3)
-      System.out.println(y);
-    else  (num3 > num1 && num3 > num2)
-      System.out.println(z);
+
+   public static void main(String[] args) {
+
+      int num1,num2,num3;
+        Scanner s = new Scanner(System.in);
+        num1= s.nextInt();
+        num2= s.nextInt();
+        num3= s.nextInt();
+      if(num1 >= num2) {
+			
+	  if(num1 >= num3)
+		System.out.println(num1);
+	  else
+	     System.out.println(num3);
+      } 
+      else {
+			
+	  if(num2 >= num3)
+		System.out.println(num2);
+	  else
+		System.out.println(num3);
+      }
    }
 }
