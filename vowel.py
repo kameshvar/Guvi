92d06ea9c9dd37f954885b7886d835f57d92c18c
kameshvar <25860419+kameshvar@users.noreply.github.com>
2019-08-09 12:06:18 +0530

    Create vowel.py

---
+++ vowel.py
@@ -0,0 +1,7 @@
+val=input("");
+if val in ('a','e','i','o','u','A','E','I','O','U') :
+	print("Vowel")
+elif val in ('!','@','#','$','%','&','*','(',')') :
+	print("invalid")
+else:
+	print("Consonant")
